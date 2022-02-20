bacad62bb1a1a8c21699bd75fe88201f06759c12
YunoGasai67 <59923820+Sylveon76@users.noreply.github.com>
2022-02-19 20:02:31 -0500

    Use Enums for TopType

--- MartineApiNet/Endpoints/IRedditApi.cs
+++ MartineApiNet/Endpoints/IRedditApi.cs
@@ -4,29 +4,12 @@ using Refit;
 
 namespace MartineApiNet.Endpoints 
 {
-  public class TopType 
+  public enum TopType
   {
-
-    /// <summary>
-    /// Get all posts
-    /// </summary>
-    public const string All = "all";
-    /// <summary>
-    /// Get posts from the past year
-    /// </summary>
-    public const string Year = "year";
-    /// <summary>
-    /// Get posts from the past month
-    /// </summary>
-    public const string Month = "month";
-    /// <summary>
-    /// Get posts from the past week
-    /// </summary>
-    public const string Week = "week";
-    /// <summary>
-    /// get posts from the last 24 hours
-    /// </summary>
-    public const string Day = "Day";
+    Year,
+    Month,
+    Week,
+    Day
   }
   /// <summary>
   /// Query the reddit api for memes, subreddits, and wallpapers!
@@ -38,7 +21,7 @@ namespace MartineApiNet.Endpoints
     /// <param name="type">Sets the range to look in.</param>
     /// <returns>Reddit Post</returns>
     [Get("/images/memes")]
-    Task<RedditPost> GetRandomMeme(TopType type = null);
+    Task<RedditPost> GetRandomMeme(TopType type = TopType.Week);
     
     /// <summary>
     /// Gets a random wallpaper from reddit
@@ -46,7 +29,7 @@ namespace MartineApiNet.Endpoints
     /// <param name="type">Sets the range to look in.</param>
     /// <returns>Reddit Post</returns>
     [Get("/images/wallpaper")]
-    Task<RedditPost> GetRandomWallpaper(TopType type = null);
+    Task<RedditPost> GetRandomWallpaper(TopType type = TopType.Week);
 
     /// <summary>
     /// Gets a random wallpaper from reddit
@@ -55,7 +38,7 @@ namespace MartineApiNet.Endpoints
     /// <param name="type">Sets the range to look in.</param>
     /// <returns>Reddit Post</returns>
     [Get("/images/subreddit")]
-    Task<RedditPost> GetRandomFromSubreddit(string subredditName, TopType type = null);
+    Task<RedditPost> GetRandomFromSubreddit(string subredditName, TopType type = TopType.Week);
     
   }
 }
